8dd25d4cd0f89ecdca614fded252d9950878230c
Alexander Udalov <udalov.alexander@gmail.com>
2013-03-02 17:58:00 +0400

    A minimal testing framework + results for Foundation

--- asserts.h
+++ asserts.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#define fail(msg) fprintf(stderr, "Assertion failed: %s (%s:%d)\n", msg, __FILE__, __LINE__), exit(1)
-#define assertWithMessage(condition, message) do { if (!(condition)) fail(message); } while (0)
+#define failWithMsg(...) fprintf(stderr, __VA_ARGS__), exit(1)
+#define failAssert(msg) failWithMsg("Assertion failed: %s (%s:%d)\n", msg, __FILE__, __LINE__)
+#define assertWithMessage(condition, message) do { if (!(condition)) failAssert(message); } while (0)
 #define assertNotNull(o) assertWithMessage(o, "'" #o "' cannot be null")
 #define assertTrue(cond) assertWithMessage(cond, "'" #cond "' should be true")
 #define assertFalse(cond) assertWithMessage(cond, "'" #cond "' should be false")
