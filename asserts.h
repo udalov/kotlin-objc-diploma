cdc21cae8c8d91419c92ad4c0a339f6856c81a82
Alexander Udalov <udalov.alexander@gmail.com>
2013-03-04 02:10:22 +0400

    Fix wrong assert macro

--- asserts.h
+++ asserts.h
@@ -5,5 +5,5 @@
 #define assertWithMessage(condition, message) do { if (!(condition)) failAssert(message); } while (0)
 #define assertNotNull(o) assertWithMessage(o, "'" #o "' cannot be null")
 #define assertTrue(cond) assertWithMessage(cond, "'" #cond "' should be true")
-#define assertFalse(cond) assertWithMessage(cond, "'" #cond "' should be false")
+#define assertFalse(cond) assertWithMessage(!cond, "'" #cond "' should be false")
 #define assertEquals(o1, o2) assertWithMessage((o1) == (o2), "'" #o1 "' is not equal to '" #o2 "'")
